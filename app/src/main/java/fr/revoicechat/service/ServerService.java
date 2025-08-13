805ec34ec472c37468583d9c198c86923a05df6e
Charles Lgn <charles.ligony@gmail.com>
2025-08-13 22:34:37 +0200

    add log to retrieve server

--- app/src/main/java/fr/revoicechat/service/ServerService.java
+++ app/src/main/java/fr/revoicechat/service/ServerService.java
@@ -3,6 +3,8 @@ package fr.revoicechat.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import fr.revoicechat.model.Server;
@@ -11,6 +13,7 @@ import fr.revoicechat.service.server.ServerProviderService;
 
 @Service
 public class ServerService {
+  private static final Logger LOG = LoggerFactory.getLogger(ServerService.class);
 
   private final ServerProviderService serverProviderService;
   private final ServerRepository serverRepository;
@@ -21,7 +24,9 @@ public class ServerService {
   }
 
   public List<Server> getAll() {
-    return serverProviderService.getServers();
+    var servs = serverProviderService.getServers();
+    LOG.info("servers LIST / {}", servs);
+    return servs;
   }
 
   public Server get(final UUID id) {
