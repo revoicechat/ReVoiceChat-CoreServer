fe0674c14c995d44437c286003827e94c1450cdd
Charles Lgn <charles.ligony@gmail.com>
2025-08-17 13:28:50 +0200

    enable swagger ui when you are not connected
    add html/json response for forbiden access

--- app/src/main/java/fr/revoicechat/web/api/LoggedApi.java
+++ app/src/main/java/fr/revoicechat/web/api/LoggedApi.java
@@ -1,19 +1,41 @@
 package fr.revoicechat.web.api;
 
+import org.springframework.http.MediaType;
+
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@ApiResponses({
-    @ApiResponse(
-        responseCode = "401",
-        description = "Unauthorized – user not logged in",
-        content = @Content(
-            mediaType = "text/plain",
-            schema = @Schema(type = "string", example = "user not logged in")
+@ApiResponse(
+    responseCode = "401",
+    description = "Unauthorized – user not logged in",
+    content = {
+        @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(type = "string", example = """
+                {
+                  "error": "Forbidden",
+                  "message": "You do not have permission to access this resource.",
+                  "swaggerDoc": "/swagger-ui/index.html"
+                }""")
+        ),
+        @Content(
+            schema = @Schema(type = "string", example = """
+                <!DOCTYPE>
+                <html lang="en">
+                <head>
+                    <title>403 Forbidden</title>
+                </head>
+                <body>
+                <div class="container">
+                    <h1>Access Denied</h1>
+                    <p>You do not have permission to access this resource.</p>
+                    <p>See the <a href="/swagger-ui/index.html">API documentation</a>.</p>
+                </div>
+                </body>
+                </html>""")
         )
-    )
-})
+    }
+)
 public interface LoggedApi {
 }
