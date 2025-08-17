7542ad5a9ee442f8d1357f630881a7915c1660bc
Charles Lgn <charles.ligony@gmail.com>
2025-08-17 13:28:50 +0200

    enable swagger ui when you are not connected
    add html/json response for forbiden access

--- app/src/main/java/fr/revoicechat/security/SecurityConfig.java
+++ app/src/main/java/fr/revoicechat/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package fr.revoicechat.security;
 
+import static jakarta.servlet.http.HttpServletResponse.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,15 +10,21 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 public class SecurityConfig {
 
-  private final UsernameOnlyAuthenticationProvider authenticationProvider;
+  private static final String[] SWAGGER_WHITELIST = { "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**" };
 
-  public SecurityConfig(UsernameOnlyAuthenticationProvider authenticationProvider) {
+  private final UsernameOnlyAuthenticationProvider authenticationProvider;
+  private final ContentAwareAccessDeniedHandler contentAwareAccessDeniedHandler;
+
+  public SecurityConfig(UsernameOnlyAuthenticationProvider authenticationProvider, final ContentAwareAccessDeniedHandler contentAwareAccessDeniedHandler) {
     this.authenticationProvider = authenticationProvider;
+    this.contentAwareAccessDeniedHandler = contentAwareAccessDeniedHandler;
   }
 
   @Bean
@@ -30,9 +38,22 @@ public class SecurityConfig {
                .cors(Customizer.withDefaults())
                .authorizeHttpRequests(auth -> auth
                    .requestMatchers("/signup", "/login").permitAll()
+                   .requestMatchers(SWAGGER_WHITELIST).permitAll()
                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                    .anyRequest().authenticated()
                )
+               .exceptionHandling(e -> e.accessDeniedHandler(handleAccessDenied())
+                                        .authenticationEntryPoint(handleUnauthorizedAccess()))
                .build();
+  }
+
+  private AccessDeniedHandler handleAccessDenied() {
+    return (request, response, ignore) ->
+        contentAwareAccessDeniedHandler.handle(request, response, SC_FORBIDDEN);
+  }
+
+  private AuthenticationEntryPoint handleUnauthorizedAccess() {
+    return (request, response, ignore) ->
+        contentAwareAccessDeniedHandler.handle(request, response, SC_UNAUTHORIZED);
   }
 }
