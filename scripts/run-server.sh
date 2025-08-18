04c57425b92aaae8a6202f285d93bbd9c197ec8e
Charles Lgn <charles.ligony@gmail.com>
2025-08-18 23:56:32 +0200

    update server script file to run with postgres

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# TODO - remove de dev profile once postgres is setup
-screen -dmS rvc-server -L -Logfile latest.log ./mvnw spring-boot:run -pl "app" -Dspring-boot.run.profiles=dev,linux
+screen -dmS rvc-server -L -Logfile latest.log ./mvnw spring-boot:run -pl "app" -Dspring-boot.run.profiles=linux,pg
