f0903cd2d98393f9b5b2436240bfebf50b7446d7
Charles Lgn <charles.ligony@gmail.com>
2025-08-25 23:07:18 +0200

    expire jwt at 30 days

--- app/src/main/java/fr/revoicechat/security/jwt/JwtService.java
+++ app/src/main/java/fr/revoicechat/security/jwt/JwtService.java
@@ -13,12 +13,14 @@ public class JwtService {
 
   @ConfigProperty(name = "mp.jwt.verify.issuer")
   String jwtIssuer;
+  @ConfigProperty(name = "revoicechat.jwt.valid-day", defaultValue = "30")
+  int jwtValidDay;
 
   public String get(final User user) {
     return Jwt.issuer(jwtIssuer)
               .subject(user.getLogin())
               .groups(Set.of("USER"))
-              .expiresAt(System.currentTimeMillis() + 3600)
+              .expiresAt(System.currentTimeMillis() + 1000L * 3600 * 24 * jwtValidDay)
               .sign();
   }
 }
