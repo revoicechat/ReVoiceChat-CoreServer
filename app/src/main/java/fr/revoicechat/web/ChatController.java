72033b96fc2a1b61f5ba49a92d1965b0ee743686
charles ligony <c-ligony@efluid.fr>
2025-08-26 09:00:13 +0200

    allow option in sse + provide jwt connexion by querry param

--- app/src/main/java/fr/revoicechat/web/ChatController.java
+++ app/src/main/java/fr/revoicechat/web/ChatController.java
@@ -1,5 +1,6 @@
 package fr.revoicechat.web;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -9,21 +10,19 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.sse.Sse;
 import jakarta.ws.rs.sse.SseEventSink;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.revoicechat.representation.sse.SseData;
 import fr.revoicechat.security.UserHolder;
 import fr.revoicechat.service.sse.TextualChatService;
 import fr.revoicechat.web.api.LoggedApi;
-import io.smallrye.common.annotation.Blocking;
-import io.smallrye.mutiny.Multi;
 
 @Path("/sse")
-@RolesAllowed("USER") // only authenticated users
+@PermitAll // allow http OPTION
 @Tag(name = "Chat", description = "Endpoints for real-time chat using Server-Sent Events (SSE)")
 public class ChatController implements LoggedApi {
   private static final Logger LOG = LoggerFactory.getLogger(ChatController.class);
@@ -36,10 +35,13 @@ public class ChatController implements LoggedApi {
     this.userHolder = userHolder;
   }
 
+  @Operation(summary = "Register a user in the notification center",
+      description = "Register a user in the notification center. a user can be registered multiple times")
   @APIResponse(responseCode = "200", description = "SSE stream successfully opened")
   @GET
   @Produces(MediaType.SERVER_SENT_EVENTS)
   @SseElementType(MediaType.APPLICATION_JSON)
+  @RolesAllowed("USER") // only authenticated users
   public void generateSseEmitter(@Context Sse sse, @Context SseEventSink sink) {
     var user = userHolder.get();
     textualChatService.register(user.getId(), sse, sink);
