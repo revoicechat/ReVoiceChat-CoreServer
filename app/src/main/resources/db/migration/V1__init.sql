9a11dce745027ab57d83d20da82f703423e08908
Charles Lgn <charles.ligony@gmail.com>
2025-08-26 22:41:41 +0200

    fix flyway initialize file

--- app/src/main/resources/db/migration/V1__init.sql
+++ app/src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 create table RVC_USER (
     createdDate timestamp(6),
-    id          varchar(50)  not null,
+    id          uuid  not null,
     displayName varchar(255) not null,
     email       varchar(255) unique,
     login       varchar(255) not null unique,
@@ -11,24 +11,24 @@ create table RVC_USER (
 );
 
 create table RVC_SERVER (
-    OWNER_ID varchar(50),
-    id       varchar(50) not null,
+    OWNER_ID uuid,
+    id       uuid not null,
     name     varchar(255),
     primary key (id),
     constraint FK_RVC_SERVER_OWNER foreign key (OWNER_ID) references RVC_USER DEFERRABLE
 );
 
 create table RVC_SERVER_USER (
-    SERVER_ID varchar(50) not null,
-    USER_ID   varchar(50) not null,
+    SERVER_ID uuid not null,
+    USER_ID   uuid not null,
     primary key (SERVER_ID, USER_ID),
     constraint FK_RVC_SERVER_USER_USER foreign key (USER_ID) references RVC_USER DEFERRABLE,
     constraint FK_RVC_SERVER_USER_SERVER foreign key (SERVER_ID) references RVC_SERVER DEFERRABLE
 );
 
 create table RVC_ROOM (
-    id        varchar(50)  not null,
-    SERVER_ID varchar(50)  not null,
+    id        uuid  not null,
+    SERVER_ID uuid  not null,
     name      varchar(255) not null,
     type      varchar(255) not null,
     primary key (id),
@@ -36,10 +36,10 @@ create table RVC_ROOM (
 );
 
 create table RVC_INVATION_LINK (
-    APPLIER_ID         varchar(50),
-    SENDER_ID          varchar(50),
-    TARGETED_SERVER_ID varchar(50),
-    id                 varchar(50) not null,
+    APPLIER_ID         uuid,
+    SENDER_ID          uuid,
+    TARGETED_SERVER_ID uuid,
+    id                 uuid not null,
     status             varchar(255),
     type               varchar(255),
     primary key (id),
@@ -50,9 +50,9 @@ create table RVC_INVATION_LINK (
 
 create table RVC_MESSAGE (
     createdDate timestamp(6),
-    ROOM_ID     varchar(50) not null,
-    USER_ID     varchar(50) not null,
-    id          varchar(50) not null,
+    ROOM_ID     uuid not null,
+    USER_ID     uuid not null,
+    id          uuid not null,
     text        text,
     primary key (id),
     constraint FK_RVC_MESSAGE_ROOM foreign key (ROOM_ID) references RVC_ROOM DEFERRABLE,
@@ -60,7 +60,7 @@ create table RVC_MESSAGE (
 );
 
 create table RVC_MEDIA_DATA (
-    id     varchar(50) not null,
+    id     uuid not null,
     name   varchar(255),
     url    varchar(255),
     origin varchar(255),
@@ -70,8 +70,8 @@ create table RVC_MEDIA_DATA (
 );
 
 create table RVC_MEASSAGE_MEDIA (
-    MEASSAGE_ID varchar(50) not null,
-    MEDIA_ID    varchar(50) not null unique,
+    MEASSAGE_ID uuid not null,
+    MEDIA_ID    uuid not null unique,
     primary key (MEASSAGE_ID, MEDIA_ID),
     constraint FK_RVC_MEASSAGE_MEDIA_MEDIA_DATA foreign key (MEDIA_ID) references RVC_MEDIA_DATA DEFERRABLE,
     constraint FK_RVC_MEASSAGE_MEDIA_MESSAGE foreign key (MEASSAGE_ID) references RVC_MESSAGE DEFERRABLE
