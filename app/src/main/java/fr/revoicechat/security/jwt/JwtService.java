d2eb0c4613ffa0087958d3ce928f2c03e175c4f4
Charles Lgn <charles.ligony@gmail.com>
2025-08-26 22:15:53 +0200

    add log in jwt service

--- app/src/main/java/fr/revoicechat/security/jwt/JwtService.java
+++ app/src/main/java/fr/revoicechat/security/jwt/JwtService.java
@@ -1,14 +1,16 @@
 package fr.revoicechat.security.jwt;
 
-import jakarta.inject.Singleton;
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.revoicechat.model.User;
 import io.smallrye.jwt.build.Jwt;
+import jakarta.inject.Singleton;
 
 @Singleton
 public class JwtService {
+  private static final Logger LOG = LoggerFactory.getLogger(JwtService.class);
 
   @ConfigProperty(name = "mp.jwt.verify.issuer")
   String jwtIssuer;
@@ -16,10 +18,16 @@ public class JwtService {
   int jwtValidDay;
 
   public String get(final User user) {
-    return Jwt.issuer(jwtIssuer)
-              .subject(user.getLogin())
-              .groups(user.getType().getRoles())
-              .expiresAt(System.currentTimeMillis() + 1000L * 3600 * 24 * jwtValidDay)
-              .sign();
+    LOG.info("generate jwt token for user {}", user.getId());
+    try {
+      return Jwt.issuer(jwtIssuer)
+                .subject(user.getLogin())
+                .groups(user.getType().getRoles())
+                .expiresAt(System.currentTimeMillis() + 1000L * 3600 * 24 * jwtValidDay)
+                .sign();
+    } catch (Exception e) {
+      LOG.error("generate jwt token", e);
+      throw e;
+    }
   }
 }
