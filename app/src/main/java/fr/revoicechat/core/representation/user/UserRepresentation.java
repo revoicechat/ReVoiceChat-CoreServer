64b6bc0a1d7356d6d2ebd163bf3d9354aa319b92
Charles Lgn <charles.ligony@gmail.com>
2025-09-26 16:30:21 +0200

    add user type on the representation

--- app/src/main/java/fr/revoicechat/core/representation/user/UserRepresentation.java
+++ app/src/main/java/fr/revoicechat/core/representation/user/UserRepresentation.java
@@ -4,11 +4,13 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import fr.revoicechat.core.model.ActiveStatus;
+import fr.revoicechat.core.model.UserType;
 
 public record UserRepresentation(
     UUID id,
     String displayName,
     String login,
     OffsetDateTime createdDate,
-    ActiveStatus status
+    ActiveStatus status,
+    UserType type
 ) {}
