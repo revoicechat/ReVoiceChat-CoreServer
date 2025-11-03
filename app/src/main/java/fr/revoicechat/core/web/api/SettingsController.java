bfbc9e1a9192cba269b9d748638e361a3e433149
charles ligony <c-ligony@efluid.fr>
2025-11-03 16:12:23 +0100

    global settings

--- app/src/main/java/fr/revoicechat/core/web/api/SettingsController.java
+++ app/src/main/java/fr/revoicechat/core/web/api/SettingsController.java
@@ -2,27 +2,34 @@ package fr.revoicechat.core.web.api;
 
 import java.util.Map;
 import java.util.UUID;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import fr.revoicechat.openapi.api.LoggedApi;
+
 @Path("/settings")
 @Tag(name = "Settings", description = "Endpoints for managing settings")
-public interface SettingsController {
+public interface SettingsController extends LoggedApi  {
 
   @Operation(summary = "Get general settings", description = "Retrieve the settings of the app.")
   @APIResponse(responseCode = "200")
+  @Produces(MediaType.APPLICATION_JSON)
   @GET
   Map<String, Object> genealSetings();
 
   @Tag(name = "User")
   @Operation(summary = "Get settings of the connected user", description = "Retrieve the settings of the connected user.")
   @APIResponse(responseCode = "200")
+  @Produces(MediaType.TEXT_PLAIN)
   @GET
   @Path("/me")
   String me();
@@ -30,6 +37,7 @@ public interface SettingsController {
   @Tag(name = "User")
   @Operation(summary = "Get settings of the connected user", description = "Retrieve the settings of the connected user.")
   @APIResponse(responseCode = "200")
+  @Produces(MediaType.TEXT_PLAIN)
   @GET
   @Path("/user/{id}")
   String ofUser(@PathParam("id") UUID id);
@@ -37,6 +45,8 @@ public interface SettingsController {
   @Tag(name = "User")
   @Operation(summary = "Update settings of the connected user", description = "Update the settings of the connected user.")
   @APIResponse(responseCode = "200")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
   @PATCH
   @Path("/me")
   String me(String settings);
