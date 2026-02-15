a2d2aada43650c452b3143fca30acd6332c48a6e
Charles Lgn <charles.ligony@gmail.com>
2026-02-15 02:19:56 +0100

    feat(server): discover only public server that are not joined

--- app/src/main/java/fr/revoicechat/core/repository/UserRepository.java
+++ app/src/main/java/fr/revoicechat/core/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package fr.revoicechat.core.repository;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -11,10 +10,6 @@ public interface UserRepository {
   Stream<User> findByServers(UUID serverID);
   long count();
   Stream<User> everyone();
-
-  default List<User> findAll() {
-    return everyone().toList();
-  }
 
   Stream<User> findByRoom(UUID room);
 }
